ed5e0ff5ebfe464266751a3bb43e76c6286e5874
Bachir MOSTAFA <mostafa.bachir@gmail.com>
2025-04-27 11:17:23 -0400

    init_models

--- models/task.go
+++ models/task.go
@@ -1,1 +1,27 @@
 package models
+
+import (
+	"time"
+)
+
+type Task struct {
+	ID                uint      `gorm:"primaryKey"`
+	Titre             string    `gorm:"size:255;not null"`
+	Description       string    `gorm:"type:text"`
+	Status            string    `gorm:"size:1;not null;default:'A'"`
+
+	ProjectID         uint      `gorm:"not null"`
+	Project           Project   `gorm:"foreignKey:ProjectID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+
+	DateAjout         time.Time `gorm:"autoCreateTime"`
+	DateModification  time.Time `gorm:"autoUpdateTime"`
+	DateDebut         *time.Time
+	DateFin           *time.Time
+	DateLimite          *time.Time
+
+	InstigateurID     *uint // Juste l'ID de l'initiateur
+	ParticipantsIDs   []uint `gorm:"-" json:"participants_ids"` // Liste d'IDs d'utilisateurs participants, NON stockée en DB
+
+	ImagePath         *string
+	FilePath          *string
+}
